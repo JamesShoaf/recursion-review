46e86c3d785a8e316685551e55fc5eaedcb2ac3b
JamesShoaf <james.s.shoaf@gmail.com>
2020-02-03 17:58:39 -0800

    implement getElementsByClassName and partially implement stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
+  var body = document.body;
+  var output = [];
+
+  var pushNodeifClassPresent = function (currentNode, className) {
+    if (_.contains(currentNode.classList, className)) {
+      output.push(currentNode);
+    }
+
+    if (currentNode.hasChildNodes()) {
+      for (let i = 0; i < currentNode.childNodes.length; i++) {
+        pushNodeifClassPresent(currentNode.childNodes[i], className);
+      }
+    }
+  };
+
+  pushNodeifClassPresent(body, className);
+  return output;
   // your code here
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //num
+  //string
+  //undefined
+  //array
+  //object
+
+  var result = '';
+
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return result = `${obj}`;
+  }
+
+  if (typeof obj === 'string') {
+    return result += `"${obj}"`;
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+
+    result += '[';
+    for (let i = 0; i < obj.length - 1; i++) {
+      result += `${stringifyJSON(obj[i])},`;
+    }
+    result += stringifyJSON(obj[obj.length - 1]) + ']';
+    return result;
+  }
+
+  if (obj instanceof Object) {
+    var keyArr = Object.keys(obj);
+
+    if (keyArr.length === 0) {
+      return '{}';
+    }
+
+    result += '{';
+
+    for (let i = 0; i < keyArr.length - 1; i++) {
+      result += `"${keyArr[i]}": ${stringifyJSON(obj[i])},`;
+    }
+    result += `"${keyArr[keyArr.length - 1]}": ${stringifyJSON(obj[keyArr.length - 1])}"}"`;
+    return result;
+  }
+
+
 };
